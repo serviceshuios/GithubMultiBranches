ee91956eadd9a353d9f0a0f87785f001ca5efb2a
serviceshuios <services.huios@gmail.com>
2016-10-11 11:39:09 +0200

    Ajoutez des fichiers projet.

---
+++ GithubBranches/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GithubBranches.Startup))]
+namespace GithubBranches
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
